c2be0b0cc724b16574aca6e6d6b7ccdca399b776
Tyler Ingram <tylerphx@gmail.com>
2021-03-21 20:56:33 -0500

    Removed unused directive

--- Exam2/Form1.cs
+++ Exam2/Form1.cs
@@ -1,11 +1,9 @@
 using System;
 using System.Windows.Forms;
 using System.IO;
-using System.Text;
-
 namespace Exam2
 {
-    //Made by Tyler Ingram in 2021 - C# Visual Prog 1
+    //Made by Tyler Ingram in 2021 - C# Visual Prog 1 Exam 2
     public partial class Form1 : Form
     {
         public Form1()
